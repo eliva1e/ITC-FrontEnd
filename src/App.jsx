cac1de5c38f3b8be034cf8799aefa097a5804a41
Kirill Siukhin <kirillsiukhin@proton.me>
2025-11-06 15:30:44 +0500

    Add redux and counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decremented, incremented } from './stores/counter';
 
 function App() {
-  const [clicks, setClicks] = useState(0);
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.value);
 
   return (
-    <button onClick={() => setClicks(clicks + 1)}>Clicks: {clicks}</button>
+    <div>
+      <h1>{value}</h1>
+      <button onClick={() => dispatch(incremented())}>Increment by 1</button>
+      <button onClick={() => dispatch(decremented())}>Decrement by 1</button>
+    </div>
   );
 }
 
---
+++ src/stores/counter.js
@@ -0,0 +1,22 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    incremented: (state) => {
+      state.value++;
+    },
+    decremented: (state) => {
+      state.value--;
+    },
+  },
+});
+
+export const { incremented, decremented } = counterSlice.actions;
+
+export const store = configureStore({
+  reducer: counterSlice.reducer,
+});
