c7e79c936a41f0d671130c58580ab6070063c685
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-09 12:36:33 +0500

    feat: list command

--- week-09-10/src/services/commands/add.ts
+++ week-09-10/src/services/commands/add.ts
@@ -3,9 +3,11 @@ import type { Command } from '../types';
 export const add: Command = {
   accept(...argv) {
     const [name] = argv;
-    return argv.length === 2 && name === 'add';
+    return argv.length >= 1 && name === 'add';
   },
-  run() {
+  run(manager, ...argv) {
+    const title = argv.slice(1).join(' ');
+    manager.addTask(title);
     return true;
   },
   description: 'add [long title]: add new task',
--- week-09-10/src/services/commands/list.ts
+++ week-09-10/src/services/commands/list.ts
@@ -5,7 +5,18 @@ export const list: Command = {
     const [name, all] = argv;
     return argv.length <= 2 && ['ls', 'list'].includes(name) && ['-a', undefined].includes(all);
   },
-  run() {
+  run(manager, ...argv) {
+    const showAll = argv[1] === '-a';
+
+    const tasks = manager.availableTasks();
+    for (const t of tasks) {
+      if (showAll) {
+        console.log(`${t.id} ${t.complete ? '[DONE]' : '[TODO]'} ${t.title}`);
+      } else if (!t.complete) {
+        console.log(`${t.id} [TODO] ${t.title}`);
+      }
+    }
+
     return false;
   },
   description: 'ls, list [-a]: list available [all] todo tasks',
